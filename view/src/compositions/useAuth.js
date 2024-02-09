cbe53d5cf0a3708558e09813b166bcd7cb3d8bc3
Trysten M <nick.tristen22@gmail.com>
2024-02-09 08:16:38 +0200

    update(view/composable) update useAuth.js

--- view/src/compositions/useAuth.js
+++ view/src/compositions/useAuth.js
@@ -1,7 +1,14 @@
 import {computed, reactive} from "vue";
 
+import {useRequest} from "./useRequest.js";
+import {useAccessToken} from "./useAccessToken.js";
+
+const {setAccessToken} = useAccessToken()
+const {apiRequest} = useRequest()
+
 const state = reactive({
-    isAuth: false
+    isAuth: false,
+    errors: []
 })
 
 export function useAuth() {
@@ -13,5 +20,34 @@ export function useAuth() {
         state.isAuth = value
     }
 
-    return {isAuth, setIsAuth}
+    async function loginRequest(formData) {
+        return await apiRequest('POST', '/login', formData)
+            .then(({data}) => {
+                setErrors([])
+                setAccessToken(data?.data?.accessToken)
+
+                return data?.data
+            })
+            .catch(res => {
+                const errors = res?.response?.data?.errors
+
+                if (errors) setErrors(errors)
+            })
+    }
+
+    const errors = computed(() => {
+        return state.errors
+    })
+
+    function setErrors(errors) {
+        state.errors = errors
+    }
+
+    return {
+        isAuth,
+        setIsAuth,
+        loginRequest,
+        errors,
+        setErrors
+    }
 }
