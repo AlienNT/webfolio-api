f8e7cf9efe17e4b3859664bba95b3ce4b4b7e6d8
Trysten M <nick.tristen22@gmail.com>
2024-02-09 07:32:16 +0200

    update(api/helpers) update auth helper

--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -2,9 +2,10 @@ import config from "../config/index.js";
 import jwt from "jsonwebtoken";
 import {randomUUID} from "node:crypto"
 import bcrypt from "bcrypt"
+
 export function generateAccessToken(_id) {
     const payload = {_id}
-    return jwt.sign(payload, config.SECRET_KEY, {expiresIn: process.env.PROD ? '2m' : '30d'})
+    return jwt.sign(payload, config.SECRET_KEY, {expiresIn: process.env.PROD ? '2m' : '1m'})
 }
 
 export function generateRefreshToken() {
@@ -19,18 +20,29 @@ export const decryptTokenData = (req) => {
 }
 
 export function verifyUser(token) {
-    return jwt.verify(token, config.SECRET_KEY);
+    try {
+        return jwt.verify(token, config.SECRET_KEY);
+    } catch (e) {
+        console.log('token expired')
+        return null
+    }
 }
 
 export function getToken(req) {
     const authorizationHeader = req.headers.authorization
     return authorizationHeader ? authorizationHeader.split(' ')[1] : null
 }
+
+export function getCookie(req, cookieName) {
+    return req?.cookies[cookieName]
+}
+
 export function getPublicAuthFields(user) {
     const {_id} = user
 
     return {_id}
 }
+
 export function getHash(string) {
     return bcrypt.hashSync(string, 7)
 }
