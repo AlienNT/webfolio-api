59c89707faf678921d744d6555b7d3634f7ffc60
Trysten M <nick.tristen22@gmail.com>
2024-02-09 08:20:38 +0200

    update(view/main) add auth actions

--- view/src/components/TheMain.vue
+++ view/src/components/TheMain.vue
@@ -1,11 +1,42 @@
 <script setup>
+import {useAuth} from "../compositions/useAuth.js";
+import {useAccessToken} from "../compositions/useAccessToken.js";
+import {onMounted, watch} from "vue";
+
+const {setIsAuth} = useAuth()
+
+const {
+  accessToken,
+  setAccessToken,
+  setTokenToLocalStorage,
+  deleteTokenInLocalStorage,
+  getTokenInLocalStorage
+} = useAccessToken()
+
+onMounted(() => {
+  const localToken = getTokenInLocalStorage()
+
+  if (!accessToken.value && localToken) {
+    setAccessToken(localToken)
+  }
+})
+
+watch(() => accessToken.value, (value, oldValue) => {
+  setIsAuth(!!value)
+
+  if (value && value !== oldValue) {
+    setAccessToken(value)
+    setTokenToLocalStorage(value)
+
+    return null
+  }
+
+  deleteTokenInLocalStorage()
+})
+
 
 </script>
 
 <template>
   <router-view/>
 </template>
-
-<style scoped>
-
-</style>
