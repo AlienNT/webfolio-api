715c4a7668d01ebc0d085ea486be68e3cf710ae2
Trysten M <nick.tristen22@gmail.com>
2024-02-09 07:28:14 +0200

    update(api/controllers) update token controller

--- controllers/tokenController.js
+++ controllers/tokenController.js
@@ -1,48 +1,64 @@
-import {errorResponse} from "../helpers/responseHelper.js";
+import {Token} from "../models/index.js";
+import {generateAccessToken, generateRefreshToken} from "../helpers/authHelper.js";
 
 class TokenController {
-    async get(req, res) {
+    async get(token) {
         try {
-
+            return await Token.findOne({value: token})
         } catch (e) {
-            console.log(e)
-            return errorResponse(res, {
-                errors: 'error'
-            })
+            console.log('TokenController GET error', e)
+            return null
         }
     }
 
     async create(req, res) {
         try {
+            const {userId} = req
+            const {remoteAddress} = req.socket
+            const userAgent = req.headers['user-agent']
 
-        } catch (e) {
-            console.log(e)
-            return errorResponse(res, {
-                errors: 'error'
+            const tokens = await Token
+                .find({user: userId})
+                .sort({orderNumber: 1})
+
+            if (tokens?.length >= 5) {
+                const oldestToken = tokens[0]
+
+                const deletedToken = await Token.deleteOne(oldestToken._id)
+
+                if (!deletedToken) {
+                    return null
+                }
+            }
+
+            const refreshToken = generateRefreshToken()
+            const accessToken = generateAccessToken(userId)
+
+            const {value} = await Token.create({
+                value: refreshToken,
+                user: userId,
+                ipAddress: remoteAddress,
+                userAgent
             })
+            return value ? {
+                refreshToken,
+                accessToken
+            } : null
+        } catch (e) {
+            console.log('TokenController CREATE error', e)
+            return null
         }
     }
 
-    async update(req, res) {
+    deleteRefreshToken(req) {
         try {
-
+            const targetRefreshToken = req.cookies?.refreshToken
+            return Token.findOneAndDelete({value: targetRefreshToken})
         } catch (e) {
-            console.log(e)
-            return errorResponse(res, {
-                errors: 'error'
-            })
-        }
-    }
-
-    async delete(req, res) {
-        try {
-
-        } catch (e) {
-            console.log(e)
-            return errorResponse(res, {
-                errors: 'error'
-            })
+            console.log('TokenController DELETE error', e)
+            return null
         }
     }
 }
+
 export default new TokenController()
