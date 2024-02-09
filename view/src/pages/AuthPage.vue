b35397786536380b4cac034f5bc0def8782ab77e
Trysten M <nick.tristen22@gmail.com>
2024-02-09 08:17:51 +0200

    update(view/pages) add login action & redirect after correct auth

--- view/src/pages/AuthPage.vue
+++ view/src/pages/AuthPage.vue
@@ -2,13 +2,35 @@
 
 import AuthForm from "../components/auth/AuthForm.vue";
 import PageTemplate from "../templates/PageTemplate.vue";
+
+import {reactive} from "vue";
+import {useAuth} from "../compositions/useAuth.js";
+import {useRouter} from "vue-router";
+
+import routerHelper from "../router/routerHelper.js";
+
+const {loginRequest} = useAuth()
+const router = useRouter()
+
+const state = reactive({
+  errors: []
+})
+
+async function login(formData) {
+  await loginRequest(formData).then(() => router.push(routerHelper.DEFAULT.path))
+}
 </script>
 
 <template>
   <section class="auth-page">
     <PageTemplate style-prefix="auth">
       <template v-slot:content>
-        <AuthForm/>
+        <AuthForm
+            @on-submit="login"
+        />
+        <div class="errors-wrapper">
+          <div v-for="error in state.errors"> {{ error }}</div>
+        </div>
       </template>
     </PageTemplate>
   </section>
@@ -19,6 +41,10 @@ import PageTemplate from "../templates/PageTemplate.vue";
   display: flex;
   align-items: center;
   height: 100%;
+}
+.errors-wrapper {
+  text-align: center;
+  color: #ff4d4d;
 }
 
 </style>
