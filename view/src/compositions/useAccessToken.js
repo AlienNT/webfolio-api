e07169a26bca1f45c3c9982bb2d0023cc9c9bb62
Trysten M <nick.tristen22@gmail.com>
2024-02-09 08:16:08 +0200

    add(view/composable) add useAccessToken.js

---
+++ view/src/compositions/useAccessToken.js
@@ -0,0 +1,38 @@
+import {computed, reactive} from "vue";
+
+const state = reactive({
+    accessToken: null
+})
+
+export function useAccessToken() {
+    const accessToken = computed(() => state.accessToken)
+
+    function setAccessToken(token) {
+        return state.accessToken = token
+    }
+
+    function removeAccessToken() {
+        return setAccessToken(null)
+    }
+
+    function setTokenToLocalStorage(token) {
+        return localStorage.setItem('accessToken', token)
+    }
+
+    function getTokenInLocalStorage() {
+        return localStorage.getItem('accessToken')
+    }
+
+    function deleteTokenInLocalStorage() {
+        return localStorage.removeItem('accessToken')
+    }
+
+    return {
+        accessToken,
+        setAccessToken,
+        removeAccessToken,
+        setTokenToLocalStorage,
+        getTokenInLocalStorage,
+        deleteTokenInLocalStorage,
+    }
+}
