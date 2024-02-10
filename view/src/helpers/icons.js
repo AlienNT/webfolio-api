e1a17a52447a0091a3d8053b8ff52a0bac285a35
Trysten M <nick.tristen22@gmail.com>
2024-02-10 17:51:10 +0200

    add(view/helpers) add icon helper

---
+++ view/src/helpers/icons.js
@@ -0,0 +1,5 @@
+const iconPath = import.meta.env.BASE_URL || '/' + 'assets/images'
+export const iconName = {
+    EDIT: iconPath + 'edit-1-svgrepo-com.svg',
+    DELETE: iconPath + 'delete-2-svgrepo-com.svg'
+}
