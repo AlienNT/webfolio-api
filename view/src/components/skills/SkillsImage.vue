d431b6823149ef1ace28535fade2518618c16184
Trysten M <nick.tristen22@gmail.com>
2024-02-10 17:59:48 +0200

    add(view/skills) add skill image

---
+++ view/src/components/skills/SkillsImage.vue
@@ -0,0 +1,22 @@
+<script setup>
+import VImage from "@/components/UI/VImage.vue";
+
+const props = defineProps({
+  src: {
+    type: String,
+    default: null
+  }
+})
+</script>
+
+<template>
+  <div class="skills-image">
+    <VImage
+        :src="src"
+    />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
