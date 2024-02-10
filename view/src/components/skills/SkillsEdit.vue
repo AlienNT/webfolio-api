224a394ec51d94991f2e0e57273a0fff628fdd46
Trysten M <nick.tristen22@gmail.com>
2024-02-10 17:59:37 +0200

    add(view/skills) add skill edit

---
+++ view/src/components/skills/SkillsEdit.vue
@@ -0,0 +1,30 @@
+<script setup>
+import SkillsForm from "@/components/skills/SkillsForm.vue";
+
+import {useRoute} from "vue-router";
+import {useSkills} from "@/compositions/state/useSkills.js";
+import {computed} from "vue";
+
+const {skill, updateField} = useSkills()
+const route = useRoute()
+
+const id = computed(() => route.params.id)
+const skillData = computed(() => skill(id.value))
+
+function changeField({fieldName, value}) {
+  updateField(id.value, fieldName, value)
+}
+</script>
+
+<template>
+<div class="skill-edit">
+  <SkillsForm
+      :skill-data="skillData.value"
+      @onChange="changeField"
+  />
+</div>
+</template>
+
+<style scoped>
+
+</style>
