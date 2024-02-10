1ceae95b54445a44c04f395c484630c31332e66e
Trysten M <nick.tristen22@gmail.com>
2024-02-10 17:53:11 +0200

    update(view/router) add child routes to skill route

--- view/src/router/routes.js
+++ view/src/router/routes.js
@@ -1,30 +1,37 @@
-import routerHelper from "./routerHelper.js";
+import routerHelper from "@/router/routerHelper.js";
 
 const routes = [
     {
         path: routerHelper.PROFILE.path,
         name: routerHelper.PROFILE.name,
-        component: () => import('../pages/ProfilePage.vue')
-    },
-    {
-        path: routerHelper.PROFILE.path,
-        name: routerHelper.PROFILE.name,
-        component: () => import('../pages/ProfilePage.vue')
+        component: () => import('@/pages/ProfilePage.vue')
     },
     {
         path: routerHelper.SKILLS.path,
         name: routerHelper.SKILLS.name,
-        component: () => import('../pages/SkillsPage.vue')
+        component: () => import('@/pages/SkillsPage.vue'),
+        children: [
+            {
+                path: routerHelper.SKILL_ADD.path,
+                name: routerHelper.SKILL_ADD.name,
+                component: () => import('@/components/skills/SkillsAdd.vue')
+            },
+            {
+                path: routerHelper.SKILL_EDIT.path,
+                name: routerHelper.SKILL_EDIT.name,
+                component: () => import('@/components/skills/SkillsEdit.vue')
+            }
+        ]
     },
     {
         path: routerHelper.WORKS.path,
         name: routerHelper.WORKS.name,
-        component: () => import('../pages/WorksPage.vue')
+        component: () => import('@/pages/WorksPage.vue')
     },
     {
         path: routerHelper.AUTH.path,
         name: routerHelper.AUTH.name,
-        component: () => import('../pages/AuthPage.vue')
+        component: () => import('@/pages/AuthPage.vue')
     },
 ]
 export default routes
