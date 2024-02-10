c489a633a24ffcef7d046a20cda6246c4ba8f28b
Trysten M <nick.tristen22@gmail.com>
2024-02-10 17:59:02 +0200

    add(api/services) add field service (test)

--- services/fieldsService.js
+++ services/fieldsService.js
@@ -1,7 +1,19 @@
+import {fieldName} from "../helpers/fieldName.js";
+
 class FieldsService {
     getSkillFields(req) {
-        const {isActive, image, title, name} = req.body
-        return {isActive, image, title, name}
+        // const {active, image, title} = req.body
+        // return {active, image, title}
+        return this.getFields(fieldName.SKILL, req)
+    }
+    getWorkFields(req){
+        return this.getFields(fieldName.WORK, req)
+    }
+
+    getFields(fieldsConfig, req){
+        const result = Object.keys(fieldsConfig).map(key => req.body?.[fieldsConfig[key]])
+        console.log('result', result)
+        return result
     }
 }
 
