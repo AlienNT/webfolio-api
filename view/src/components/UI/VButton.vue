34f50bc69d791690b14f86c33c9727449ecc42c4
Trysten M <nick.tristen22@gmail.com>
2024-02-10 17:48:30 +0200

    add(view/UI) add UI button

---
+++ view/src/components/UI/VButton.vue
@@ -0,0 +1,40 @@
+<script setup>
+defineProps({
+  label: {
+    type: String,
+    default: 'Button'
+  },
+  type: {
+    type: String,
+    default: 'button'
+  },
+  customClass: {
+    type: String
+  },
+  disabled: {
+    type: Boolean,
+    default: false
+  }
+})
+const emit = defineEmits(['onClick'])
+</script>
+
+<template>
+  <button
+    class="button"
+    :class="customClass"
+    :disabled="disabled"
+    :type="type"
+    @click="emit('onClick')"
+  >
+    <span class="label">{{ label }}</span>
+  </button>
+</template>
+
+<style scoped lang="scss">
+.button {
+  padding: 5px 15px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+</style>
