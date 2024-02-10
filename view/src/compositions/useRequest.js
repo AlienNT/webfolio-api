f818c668548cde7dd8da3644f2eb74c66a42a00b
Trysten M <nick.tristen22@gmail.com>
2024-02-10 17:50:30 +0200

    update(view/api) update api request

--- view/src/compositions/useRequest.js
+++ view/src/compositions/useRequest.js
@@ -5,21 +5,28 @@ import {useAccessToken} from "./useAccessToken.js";
 const {accessToken} = useAccessToken()
 
 export function useRequest() {
-    // axios.defaults.withCredentials = true
+    axios.defaults.withCredentials = true
     axios.defaults.headers = {
         Authorization: `Bearer ${accessToken.value}`,
     }
-    async function apiRequest(method = 'GET', route, body) {
+
+    async function apiRequest({method = 'GET', route, body}) {
         return axios({
             method,
             baseURL: config.HOST.dev,
             url: route,
             data: body
         }, {
+            withCredentials: true,
             headers: {
                 Authorization: `Bearer ${accessToken.value}`,
             }
-        })
+        }).then(({data, status}) => {
+            return {
+                data: data?.data,
+                status
+            }
+        }).catch(err => err)
     }
 
     return {
