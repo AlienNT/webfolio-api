cdbe333dd1d1e04d84426c806330c06570746725
Trysten M <nick.tristen22@gmail.com>
2024-02-10 17:52:08 +0200

    update(view/pages) show shill list in skill page

--- view/src/pages/SkillsPage.vue
+++ view/src/pages/SkillsPage.vue
@@ -1,12 +1,43 @@
 <script setup>
-
 import PageTemplate from "../templates/PageTemplate.vue";
+import SkillsList from "@/components/skills/SkillsList.vue";
+
+import {useSkillRequest} from "../compositions/api/useSkillRequest.js";
+import {useSkills} from "../compositions/state/useSkills.js";
+import {onMounted} from "vue";
+
+const {fetchSkills} = useSkillRequest()
+const {skills} = useSkills()
+
+onMounted(() => {
+  if (!skills.value?.length) fetchSkills()
+})
+
+function editHandler(e) {
+  console.log('editHandler', e)
+}
+function deleteHandler(e) {
+  console.log('deleteHandler', e)
+}
+function toggleActiveHandler(e) {
+  console.log('toggleActiveHandler', e)
+}
+
+
 </script>
 
 <template>
   <PageTemplate style-prefix="skills">
     <template v-slot:content>
-      skills
+      <div class="form">
+        <router-view/>
+      </div>
+      <SkillsList
+          :skills-list="skills"
+          @on-edit="editHandler"
+          @on-delete="deleteHandler"
+          @on-click="toggleActiveHandler"
+      />
     </template>
   </PageTemplate>
 </template>
