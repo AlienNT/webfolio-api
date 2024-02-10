31904f8bd4995956f7475abcc4f03880c7525555
Trysten M <nick.tristen22@gmail.com>
2024-02-10 17:47:03 +0200

    add(view/config) set alias

--- view/vite.config.js
+++ view/vite.config.js
@@ -1,6 +1,7 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import autoprefixer from 'autoprefixer'
+import {fileURLToPath, URL} from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,6 +20,11 @@ export default defineConfig({
                 @import "src/assets/css/mixin.scss";
                 `
             }
+        }
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     }
 })
