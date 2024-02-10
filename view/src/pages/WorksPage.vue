39e99841326ac78962af83d91d32f2000e23d81b
Trysten M <nick.tristen22@gmail.com>
2024-02-10 17:52:29 +0200

    update(view/pages) show work in work page

--- view/src/pages/WorksPage.vue
+++ view/src/pages/WorksPage.vue
@@ -1,12 +1,21 @@
 <script setup>
-
 import PageTemplate from "../templates/PageTemplate.vue";
+import {useWorkRequest} from "@/compositions/api/useWorkRequest.js";
+import {useWorks} from "@/compositions/state/useWorks.js";
+import {onMounted} from "vue";
+
+const {fetchWorks} = useWorkRequest()
+const {works} = useWorks()
+
+onMounted(() => {
+  if (!works.value?.length) fetchWorks()
+})
 </script>
 
 <template>
   <PageTemplate style-prefix="works">
     <template v-slot:content>
-      works
+      {{ works }}
     </template>
   </PageTemplate>
 </template>
