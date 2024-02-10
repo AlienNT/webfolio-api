55f70af9a9afcbc911c1907696c717a7c2c17a08
Trysten M <nick.tristen22@gmail.com>
2024-02-10 17:52:47 +0200

    update(view/router) update router helper

--- view/src/router/routerHelper.js
+++ view/src/router/routerHelper.js
@@ -15,6 +15,14 @@ export default {
         path: '/skills',
         name: 'skills'
     },
+    SKILL_ADD: {
+        path: '',
+        name: 'skillAdd'
+    },
+    SKILL_EDIT: {
+        path: 'edit/:id',
+        name: 'skillEdit'
+    },
     AUTH: {
         path: '/auth',
         name: 'auth'
