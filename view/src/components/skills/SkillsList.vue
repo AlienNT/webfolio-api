c47de9bd26d01e23667246006907af4ae655a9aa
Trysten M <nick.tristen22@gmail.com>
2024-02-10 17:59:55 +0200

    add(view/skills) add skill list

---
+++ view/src/components/skills/SkillsList.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+
+import SkillsListItem from "@/components/skills/SkillsListItem.vue";
+
+defineProps({
+  skillsList: {
+    type: Array,
+    default: null
+  }
+})
+const emit = defineEmits(['onClick', 'onDelete', 'onEdit'])
+
+</script>
+
+<template>
+  <ul class="skills-list">
+    <SkillsListItem
+        v-for="(skill, key) in skillsList"
+        :key="skill?._id || key"
+        :skill-data="skill"
+        @onClick="e => emit('onClick', e)"
+        @onDelete="e => emit('onDelete', e)"
+        @onEdit="e => emit('onEdit', e)"
+    />
+  </ul>
+</template>
+
+<style scoped>
+.skills-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
