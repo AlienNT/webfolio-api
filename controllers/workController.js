5fcc238ade6287ea6cc813f0e4d835c8c7c323d3
Trysten M <nick.tristen22@gmail.com>
2024-02-10 17:55:12 +0200

    update(api/controllers) update work controller methods

--- controllers/workController.js
+++ controllers/workController.js
@@ -1,46 +1,93 @@
-import {errorResponse} from "../helpers/responseHelper.js";
+import {errorResponse, successResponse} from "../helpers/responseHelper.js";
+import {Work} from "../models/index.js";
+import statusCode from "../helpers/statusCodeHelper.js";
+import FieldsService from "../services/fieldsService.js";
 
 class WorkController {
     async get(req, res) {
         try {
+            const works = await Work.find({})
 
+            if (!works) {
+                return errorResponse(res, {
+                    status: statusCode.NOT_FOUND,
+                    errors: ['works not found']
+                })
+            }
+            return successResponse(res, {
+                data: works
+            })
         } catch (e) {
-            console.log(e)
+            console.log('WorkController GET error', e)
             return errorResponse(res, {
-                errors: 'error'
+                errors: ['get works error']
             })
         }
     }
 
     async create(req, res) {
         try {
+            const fields = FieldsService.getWorkFields(req)
+            const newWork = await Work.create(fields)
 
+            if (!newWork) {
+                return errorResponse(res, {
+                    errors: ['user not created']
+                })
+            }
+
+            return successResponse(res, {
+                data: newWork
+            })
         } catch (e) {
-            console.log(e)
+            console.log('WorkController CREATE error', e)
             return errorResponse(res, {
-                errors: 'error'
+                errors: ['create work error']
             })
         }
     }
 
     async update(req, res) {
         try {
+            const {id} = req?.params
+            const fields = FieldsService.getSkillFields(req)
 
+            const updatedWork = await Work.findByIdAndUpdate(id, fields)
+
+            if (!updatedWork) {
+                return errorResponse(res, {
+                    errors: [`work ${id} not updated`]
+                })
+            }
+            return successResponse(res, {
+                data: updatedWork
+            })
         } catch (e) {
-            console.log(e)
+            console.log('WorkController UPDATE error', e)
             return errorResponse(res, {
-                errors: 'error'
+                errors: ['update work error']
             })
         }
     }
 
     async delete(req, res) {
         try {
+            const {id} = req.params
 
+            const deletedWork = await Work.findByIdAndDelete(id)
+
+            if (!deletedWork) {
+                return errorResponse(res, {
+                    errors: [`work ${id} not deleted`]
+                })
+            }
+            return successResponse(res, {
+                data: deletedWork
+            })
         } catch (e) {
-            console.log(e)
+            console.log('WorkController DELETE error', e)
             return errorResponse(res, {
-                errors: 'error'
+                errors: ['delete work error']
             })
         }
     }
