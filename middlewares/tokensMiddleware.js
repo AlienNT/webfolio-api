ece3cb61a6f331a37dd8410e8e6a053914901a98
Trysten M <nick.tristen22@gmail.com>
2024-02-10 17:44:01 +0200

    add(api/middlewares) add token middleware helper

---
+++ middlewares/tokensMiddleware.js
@@ -0,0 +1,25 @@
+import {getCookie, getToken} from "../helpers/authHelper.js";
+import TokenController from "../controllers/tokenController.js";
+
+export default async function (req, res, next) {
+    console.log('TOKEN MIDDLEWARE')
+    if (req.method === "OPTIONS") {
+        next()
+    }
+   try {
+       const accessToken = getToken(req)
+       const refreshToken = getCookie(req, 'refreshToken')
+
+       const savedToken = await TokenController.get(refreshToken)
+
+       req.acessToken = accessToken
+       req.refreshToken = refreshToken
+       req.savedToken = savedToken
+       req.isAdmin = !!savedToken
+
+       next()
+   } catch (e) {
+       console.log('tokenController error', e)
+       next()
+   }
+}
