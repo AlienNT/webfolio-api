d79e7f837d81bf68509c72df643247c771721bee
Trysten M <nick.tristen22@gmail.com>
2024-02-10 17:59:31 +0200

    add(view/skills) add skill add

---
+++ view/src/components/skills/SkillsAdd.vue
@@ -0,0 +1,33 @@
+<script setup>
+import SkillsForm from "@/components/skills/SkillsForm.vue";
+import {reactive} from "vue";
+
+
+const skillData = reactive({
+  title: null,
+  active: null,
+  image: null,
+})
+function clearFormFields () {
+  Object.keys(skillData).forEach(key => skillData[key] = null)
+  console.log('clearFormFields', skillData)
+}
+function changeField({fieldName, value}) {
+  skillData[fieldName] = value
+}
+</script>
+
+<template>
+<div class="skill-add">
+  <SkillsForm
+      :is-new="true"
+      :skill-data="skillData"
+      @onChange="changeField"
+      @onClear="clearFormFields"
+  />
+</div>
+</template>
+
+<style scoped>
+
+</style>
