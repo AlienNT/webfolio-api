94cf20bb7d442b916b850f1304e5a31bc19c58bb
Trysten M <nick.tristen22@gmail.com>
2024-02-11 20:51:51 +0200

    add(api/router) add refresh router

--- routes/_authRouter.js
+++ routes/_authRouter.js
@@ -5,6 +5,7 @@ import AuthController from "../controllers/authController.js";
 const router = new Router()
 
 .get('/auth', AuthController.get)
+.get('/refresh', AuthController.refresh)
 .post('/login', AuthController.login)
 .post('/registration', AuthController.registration)
 .delete('/auth', authMiddleware, AuthController.delete)
