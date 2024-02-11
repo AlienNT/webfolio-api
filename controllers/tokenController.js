dd9cabd18972ce5a36536a2f4f6600e823bca6bd
Trysten M <nick.tristen22@gmail.com>
2024-02-11 20:54:59 +0200

    update(api/controllers) update token controller

--- controllers/tokenController.js
+++ controllers/tokenController.js
@@ -1,6 +1,4 @@
 import {Token} from "../models/index.js";
-import {generateAccessToken, generateRefreshToken} from "../helpers/authHelper.js";
-import TokenService from "../services/tokenService.js";
 
 class TokenController {
     async get(token) {
@@ -8,54 +6,6 @@ class TokenController {
             return await Token.findOne({value: token})
         } catch (e) {
             console.log('TokenController GET error', e)
-            return null
-        }
-    }
-
-    async create(req, res) {
-        try {
-            const {userId} = req
-            const {remoteAddress} = req.socket
-            const userAgent = req.headers['user-agent']
-
-            const tokens = await Token
-                .find({user: userId})
-                .sort({orderNumber: 1})
-
-            if (tokens?.length >= 5) {
-                const oldestToken = tokens[0]
-
-                const deletedToken = await Token.deleteOne(oldestToken._id)
-
-                if (!deletedToken) {
-                    return null
-                }
-            }
-
-            const {refreshToken, accessToken} = TokenService.generateTokens(userId)
-
-            const {value} = await Token.create({
-                value: refreshToken,
-                user: userId,
-                ipAddress: remoteAddress,
-                userAgent
-            })
-            return value ? {
-                refreshToken,
-                accessToken
-            } : null
-        } catch (e) {
-            console.log('TokenController CREATE error', e)
-            return null
-        }
-    }
-
-    deleteRefreshToken(req) {
-        try {
-            const targetRefreshToken = req.cookies?.refreshToken
-            return Token.findOneAndDelete({value: targetRefreshToken})
-        } catch (e) {
-            console.log('TokenController DELETE error', e)
             return null
         }
     }
