86f8a9498756ba24f42bdde9a67faab839a0be3e
Trysten M <nick.tristen22@gmail.com>
2024-02-11 21:00:21 +0200

    update(view/main) add redirect to auth page

--- view/src/components/TheMain.vue
+++ view/src/components/TheMain.vue
@@ -2,8 +2,11 @@
 import {useAuth} from "../compositions/useAuth.js";
 import {useAccessToken} from "../compositions/useAccessToken.js";
 import {onMounted, watch} from "vue";
+import {useRouter} from "vue-router";
+import routerHelper from "@/router/routerHelper.js";
 
-const {setIsAuth} = useAuth()
+const {setIsAuth, logout} = useAuth()
+const router = useRouter()
 
 const {
   accessToken,
@@ -25,13 +28,16 @@ watch(() => accessToken.value, (value, oldValue) => {
   setIsAuth(!!value)
 
   if (value && value !== oldValue) {
-    setAccessToken(value)
     setTokenToLocalStorage(value)
 
     return null
   }
 
   deleteTokenInLocalStorage()
+
+  if (!accessToken.value) {
+    router.push({name: routerHelper.AUTH.name})
+  }
 })
 
 
