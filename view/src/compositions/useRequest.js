bff881997b8adac0198b1a243c4a91bc232e3a23
Trysten M <nick.tristen22@gmail.com>
2024-02-11 20:59:04 +0200

    update(view/api) update request; add auto refresh token request

--- view/src/compositions/useRequest.js
+++ view/src/compositions/useRequest.js
@@ -1,22 +1,45 @@
 import axios from "axios";
 import config from "../../config.js";
-import {useAccessToken} from "./useAccessToken.js";
 
-const {accessToken} = useAccessToken()
+import {useAccessToken} from "./useAccessToken.js";
+import {useAuth} from "@/compositions/useAuth.js";
+
+import routerHelper from "@/router/routerHelper.js";
+import {requestRoutes} from "@/helpers/requestRoutes.js";
+
+const {accessToken, setAccessToken} = useAccessToken()
+const {logout} = useAuth()
 
 export function useRequest() {
-    axios.defaults.withCredentials = true
-    axios.defaults.headers = {
-        Authorization: `Bearer ${accessToken.value}`,
+    async function apiRequest({method = 'GET', route, body}) {
+        console.log('route', route)
+        const tokenExpired = route !== routerHelper.AUTH.path ? isTokenExpired(accessToken.value) : false
+        console.log('tokenExpired', tokenExpired)
+
+        if (tokenExpired) {
+            await refreshTokenRequest()
+        }
+        return request({method, route, body});
     }
 
-    async function apiRequest({method = 'GET', route, body}) {
+    async function refreshTokenRequest() {
+        return await request({
+            method: requestRoutes.AUTH.REFRESH.method,
+            route: requestRoutes.AUTH.REFRESH.url
+
+        }).then(({data}) => {
+            setAccessToken(data?.accessToken)
+
+            return data?.accessToken
+        })
+    }
+
+    function request({method = 'GET', route, body}) {
         return axios({
             method,
             baseURL: config.HOST.dev,
             url: route,
-            data: body
-        }, {
+            data: body,
             withCredentials: true,
             headers: {
                 Authorization: `Bearer ${accessToken.value}`,
@@ -26,10 +49,34 @@ export function useRequest() {
                 data: data?.data,
                 status
             }
-        }).catch(err => err)
+        }).catch(({response}) => {
+            if (response.status === 401) {
+                logout()
+            }
+
+            return response
+        })
     }
 
     return {
         apiRequest
     }
+}
+
+function isTokenExpired(token) {
+    if (!token) return true
+
+    const base64Url = token.split(".")[1];
+    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    const jsonPayload = decodeURIComponent(
+        atob(base64)
+            .split("")
+            .map(function (c) {
+                return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+            })
+            .join("")
+    );
+
+    const {exp} = JSON.parse(jsonPayload);
+    return Date.now() >= (exp) * 1000
 }
