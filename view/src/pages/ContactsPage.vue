96f255610a434b9b1f63f1ba1a5d3f6fa4a8d38a
Trysten M <nick.tristen22@gmail.com>
2024-02-15 23:44:47 +0200

    add(view/pages) add contact page

---
+++ view/src/pages/ContactsPage.vue
@@ -0,0 +1,28 @@
+<script setup>
+import PageTemplate from "@/templates/PageTemplate.vue";
+
+import {useContacts} from "@/compositions/state/useContacts.js";
+import {useContactRequest} from "@/compositions/api/useContactRequest.js";
+import {onMounted} from "vue";
+
+const {contacts} = useContacts()
+const {fetchContacts} = useContactRequest()
+
+onMounted(() => {
+  if (!contacts.value?.length) fetchContacts()
+})
+</script>
+
+<template>
+  <section class="contacts-page">
+    <PageTemplate>
+      <template v-slot:content>
+        {{ contacts }}
+      </template>
+    </PageTemplate>
+  </section>
+</template>
+
+<style scoped lang="scss">
+
+</style>
