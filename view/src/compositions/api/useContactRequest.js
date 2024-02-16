1dc56ab7f25776a69acda0d9b14bdd8152bb30d9
Trysten M <nick.tristen22@gmail.com>
2024-02-16 23:40:42 +0200

    add(view/api) add update contact request

--- view/src/compositions/api/useContactRequest.js
+++ view/src/compositions/api/useContactRequest.js
@@ -3,7 +3,7 @@ import {useRequest} from "@/compositions/useRequest.js";
 import {useContacts} from "@/compositions/state/useContacts.js";
 const {apiRequest} = useRequest()
 
-const {setContacts} = useContacts()
+const {setContacts, updateContact, addContact, removeContact} = useContacts()
 
 export function useContactRequest () {
     async function fetchContacts() {
@@ -20,7 +20,23 @@ export function useContactRequest () {
             console.log('fetchContacts error', err)
         })
     }
+    async function patchContact({id, body}) {
+        return await apiRequest({
+            method: requestRoutes.CONTACTS.UPDATE.method,
+            route: requestRoutes.CONTACTS.UPDATE.url(id),
+            body
+
+        }).then(({data, status}) => {
+            updateContact(data)
+
+            return {data, status}
+
+        }).catch(err => {
+            console.log('patchContact error', err)
+        })
+    }
     return {
-        fetchContacts
+        fetchContacts,
+        patchContact
     }
 }
